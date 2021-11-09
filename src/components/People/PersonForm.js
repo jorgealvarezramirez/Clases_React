1f8389206fae7be4f29bd711bacdf68b697f1268
jorgealvarezramirez <jaar77@gmail.com>
2021-11-09 13:15:28 -0500

    Se contruye componente PersonForm

--- src/components/People/PersonForm.js
+++ src/components/People/PersonForm.js
@@ -4,30 +4,133 @@ class PersonForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nombre: "Hola Mundo!!!!!",
+      form: {
+        name: "",
+        lastname: "",
+        phone: "",
+        email: "",
+      },
     };
-  }
-  // Manejador de eventos
-  manejadorNombre() {
-    console.log("Click......!!!");
+    // Biendear
+    // this.handlerName = this.handlerName.bind(this);
+    // this.handlerLastname = this.handlerLastname.bind(this);
+    // this.handlerPhone = this.handlerPhone.bind(this);
+    // this.handlerEmail = this.handlerEmail.bind(this);
+    this.handlerValue = this.handlerValue.bind(this);
+    this.handlerSubmit = this.handlerSubmit.bind(this);
   }
 
+  // Manejador de eventos
+  handlerName(e) {
+    console.log("Tecleo......!!!");
+    console.log("Evento---> " + e);
+    console.log("Valor-> ", e.target.value);
+    // Modificar rl valor del estado "nombre"
+    this.setState({
+      name: e.target.value,
+    });
+  }
+
+  // handlerLastname(e) {
+  //   this.setState({
+  //     lastname: e.target.value,
+  //   });
+  // }
+  // handlerPhone(e) {
+  //   this.setState({
+  //     phone: e.target.value,
+  //   });
+  // }
+  // handlerEmail(e) {
+  //   this.setState({
+  //     email: e.target.value,
+  //   });
+  // }
+  handlerValue(e) {
+    let form = this.state.form;
+    this.setState({
+      form: { ...form, [e.target.name]: e.target.value },
+    });
+
+    // switch (e.target.name) {
+    //   case "name":
+    //     this.setState({ name: e.target.value });
+    //     break;
+    //   case "lastname":
+    //     this.setState({ lastname: e.target.value });
+    //     break;
+    //   case "phone":
+    //     this.setState({ phone: e.target.value });
+    //     break;
+    //   case "email":
+    //     this.setState({ email: e.target.value });
+    //     break;
+    //   default:
+    //     break;
+    // }
+    console.log("this.state");
+  }
+  handlerSubmit(e) {
+    console.log(e);
+    e.preventDefault();
+    console.table(this.state.form);
+  }
   render() {
     return (
       <>
-        <form>
-          <label htmlFor="nombre">Nombre:</label>
+        <form onSubmit={this.handlerSubmit}>
+          <label htmlFor="name">Nombre:</label>
           <input
-            id="nombre"
-            name="nombre"
+            id="name"
+            name="name"
             type="text"
-            value={this.state.nombre}
-            onChange={this.manejadorNombre}
+            value={this.state.form.name}
+            onChange={this.handlerValue}
             placeholder="Escribe tu nombre"
+          />
+
+          {/* {this.state.name} */}
+
+          <label htmlFor="lastname">Apellido:</label>
+          <input
+            id="lastname"
+            name="lastname"
+            type="text"
+            value={this.state.form.lastname}
+            onChange={this.handlerValue}
+            placeholder="Escribe tu apellido"
           />
           <br />
           <br />
-          {this.state.nombre}
+          {/* {this.state.lastname} */}
+
+          <label htmlFor="phone">Teléfono:</label>
+          <input
+            id="phone"
+            name="phone"
+            type="tel"
+            value={this.state.form.phone}
+            onChange={this.handlerValue}
+            placeholder="Escribe tu teléfono"
+          />
+
+          {/* {this.state.phone} */}
+
+          <label htmlFor="email">Correo:</label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            value={this.state.form.email}
+            onChange={this.handlerValue}
+            placeholder="Escribe tu correo"
+          />
+          <br />
+          <br />
+          {/* {this.state.email} */}
+          <br />
+          <button type="submit">Guardar</button>
+          <br />
         </form>
       </>
     );
